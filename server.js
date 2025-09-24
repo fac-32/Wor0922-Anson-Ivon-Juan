ff78f21263438e8f7dbb936eef3c726eb25b4181
krilklem <rafi.panyotova@gmail.com>
2025-09-24 11:04:53 +0100

    Add routing to /rafi

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const PORT = process.env.PORT || 3000;
 // Serve everything inside your project folder (HTML, CSS, JS, images)
 app.use(express.static(path.join(__dirname)));
 
+app.get("/rafi", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/rafi.html"));
+});
+
 // Fallback: send index.html if route not found (useful if you later add client-side routing)
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+  res.sendFile(path.join(__dirname, "src/index.html"));
 });
 
 // Start the server
